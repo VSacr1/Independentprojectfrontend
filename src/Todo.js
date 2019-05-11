3c231037b4df5d2295d3050ba8439751e344eb8c
Vsacre1 <vjsacre@gmail.com>
2019-05-11 22:29:05 +0100

    changing back to localhost

--- src/Todo.js
+++ src/Todo.js
@@ -34,7 +34,7 @@ export default class Todo extends Component {
 
     //R
     componentDidMount =()=>  {
-        fetch('http://10.154.0.2:8081/api/v1/todo').then(results =>{
+        fetch('http://localhost:8081/api/v1/todo').then(results =>{
             return results.json();
             
         }).then(data => {
@@ -48,7 +48,7 @@ export default class Todo extends Component {
     //C
     onSubmitHandle = (event) => {
         // event.preventDefault();
-        fetch('http://10.154.0.3:8081/api/v1/todo', {
+        fetch('http://localhost:8081/api/v1/todo', {
             method:'POST',
             headers: {
                 'Accept': 'application/json',
@@ -77,7 +77,7 @@ export default class Todo extends Component {
                 
                 })
         });
-        fetch("http://10.154.0.3:8081/api/v1/todo/" + id, {method: "DELETE"});
+        fetch("http://localhost:8081/api/v1/todo/" + id, {method: "DELETE"});
     }
 
     //U
@@ -85,7 +85,7 @@ export default class Todo extends Component {
         let id = e.target.className;
         let number = e.target.id
         console.log(id);
-        fetch('http://10.154.0.3:8081/api/v1/todo/'+ id, {
+        fetch('http://localhost:8081/api/v1/todo/'+ id, {
             method:'PUT',
             headers: {
                 'Accept': 'application/json',
